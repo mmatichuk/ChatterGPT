48e6948eb94e31bd8b1ac60cec7a6e8086b3d4a3
mmatichuk <mark.matichukW@hotmail.com>
2023-11-03 22:10:36 -0600

    init commit2

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,3 +1,5 @@
+# temp
+
 import streamlit as st
 from langchain import OpenAI
 from langchain.docstore.document import Document
@@ -26,7 +28,7 @@ txt_input = st.text_area('Enter your text', '', height=200)
 # Form to accept user's text input for summarization
 result = []
 with st.form('summarize_form', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    openai_api_key = "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" # st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
     submitted = st.form_submit_button('Submit')
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
